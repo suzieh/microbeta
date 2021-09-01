f4cb0ab6f6236befe6a913c0b4e97b73fb39ebed
Suzie Hoops <suziehoops@ra-full-mfa-10-20-40-52.vpn.umn.edu>
2021-09-01 10:41:45 -0500

    initialize Rproj

---
+++ lib/phylo_data.R
@@ -0,0 +1,9 @@
+# Parsing  Data
+# obtained from: https://github.com/Riselya/Prevalence-Thresholds-Metaanalysis
+
+# Load RDS object from data folder
+## contains human, meerkat, deer, spiny rat, carollia, mouse lemur, flamingo, stint
+## we want only the human
+phylo <- readRDS("data/phylo_list_unrarefied_frontiers.RDS")
+
+# Rarefy as done in the original paper
