25910d28a8bcbbab06f2a4330ef718d78599fe24
suzieh <shoops@umn.edu>
2022-02-28 08:31:18 -0600

    sim_results

--- lib/cachi_simulation.r
+++ lib/cachi_simulation.r
@@ -18,8 +18,10 @@ plot_dim12 <- function (pc_cmd) {
   colnames(pc_cmd)[1:2] <- c("Dim1", "Dim2")
   pc_cmd$color <- 1:nrow(pc_cmd)
   pc_cmd$sample <- as.numeric(gsub("sample.", "", 1:nrow(pc_cmd)))
+  pc_cmd$Dim1 <- -pc_cmd$Dim1
+  pc_cmd$Dim2 <- -pc_cmd$Dim2
   p <- ggplot(pc_cmd, aes(x=Dim1, y=Dim2, color=factor(color))) +
-    geom_point(size = 3) +
+    geom_point(size = 4) +
     scale_color_manual(values = unname(colors[rownames(pc_cmd)])) +
     labs(title = "", x = "Dimension 1", y = "Dimension 2") + 
     theme_classic() + 
@@ -38,13 +40,25 @@ names(colors) <- rownames(dat)
 chi_d <- vegdist(dat, method = "chisq")
 bray_d <- vegdist(dat, method = "bray")
 jacc_d <- vegdist(dat, method = "jaccard")
+euc_d <- vegdist(dat, method = "euclidean")
 # Compare distances
+par(mar=c(5.1,4.1,4.1,4.1))
 plt_chi_d <- as.matrix(chi_d); plt_chi_d[lower.tri(chi_d)] <- 0;
 plot(plt_chi_d, col = viridis, main="Chi-Square Distances")
 plt_bray_d <- as.matrix(bray_d); plt_bray_d[lower.tri(bray_d)] <- 0;
 plot(plt_bray_d, col = viridis, main="Bray-Curtis Distances")
 plt_jacc_d <- as.matrix(jacc_d); plt_jacc_d[lower.tri(jacc_d)] <- 0;
 plot(plt_jacc_d, col = viridis, main="Jaccard Distances")
+plt_euc_d <- as.matrix(euc_d); plt_euc_d[lower.tri(euc_d)] <- 0;
+plot(plt_euc_d, col = viridis, main="Euclidean Distances")
+par(mar=c(5.1,4.1,4.1,2.1))
+# Compare distances in PCA
+chi_pca <- as.data.frame(cmdscale(chi_d))
+plot_dim12(chi_pca) # exported as 500x450
+bray_pca <- as.data.frame(cmdscale(bray_d))
+plot_dim12(bray_pca) # exported as 500x450
+euc_pca <- as.data.frame(cmdscale(euc_d))
+plot_dim12(euc_pca) # exported as 500x450
 # CCA function uses Chi-Sq distance and CA via SVD
 ca_out <- cca(dat)
 # Plot results
--- lib/constrain_simulation.r
+++ lib/constrain_simulation.r
@@ -12,15 +12,17 @@ library(viridis)
 current_dir = "~/Desktop/microbeta/"
 
 ##### Helpful functions #####
-plot.pc1 <- function (pc_cmd) {
+plot_dim12 <- function (pc_cmd) {
   pc_cmd <- as.data.frame(pc_cmd)
   colnames(pc_cmd)[1:2] <- c("Dim1", "Dim2")
   pc_cmd$color <- 1:nrow(pc_cmd)
-  pc_cmd$sample <- as.numeric(gsub("sample.", "", rownames(pc_cmd)))
-  p <- ggplot(pc_cmd, aes(x=sample, y=Dim1, color=factor(color))) +
-    geom_point(size = 3) +
+  pc_cmd$sample <- as.numeric(gsub("sample.", "", 1:nrow(pc_cmd)))
+  #pc_cmd$Dim1 <- -pc_cmd$Dim1
+  #pc_cmd$Dim2 <- -pc_cmd$Dim2
+  p <- ggplot(pc_cmd, aes(x=Dim1, y=Dim2, color=factor(color))) +
+    geom_point(size = 4) +
     scale_color_manual(values = unname(colors[rownames(pc_cmd)])) +
-    labs(title = "", x = "", y = "") + 
+    labs(title = "", x = "Dimension 1", y = "Dimension 2") + 
     theme_classic() + 
     theme(legend.position = 'none')
   return(p)
@@ -28,12 +30,25 @@ plot.pc1 <- function (pc_cmd) {
 
 ##### Loading Data #####
 dat_all <- read.delim(paste0(current_dir, "data/sim_gradient/fake_rel_abun_long.txt"), sep="\t", header=T)
-dat <- dat_all[1:100,] # remove noise: 1:200, all to include noise.
+dat <- dat_all[c(rbind(seq(1,50,1),seq(51,100,1))),] # rearranging to have noise next to matching "samples".
 colors <- viridis(nrow(dat))
 names(colors) <- rownames(dat)
 
-##### Run CA on Chi-Square distances #####
-# CCA function uses Chi-Sq distance and CA via SVD
-ca_out <- cca(dat)
-# Plot results
-plot.pc1(ca_out$CA$u)
+##### Run RDA #####
+# Create a metadata variable(s)
+gradient_loc <- rep(1:50,each = 2)
+noise <- rep(0:1, times = 50)
+# RDA computation
+rda_out <- rda(dat ~ gradient_loc, scale = T) # can also add + noise
+# Biplot of RDA results
+rda_pts <- scores(rda_out)$sites
+colnames(rda_pts) <- c("Dim1", "Dim2")
+rda_arrows <- rda_out$CCA$envcentre
+p <- plot_dim12(rda_pts)
+p <- p + annotate("text", x = 2, y = 0.2, label = "gradient", col = "blue") +
+  annotate("segment", x = 0, xend = 2, y = 0, yend = 0, col = "blue", arrow = arrow(ends = "last",length = unit(.2,"cm")))
+  #annotate("text", x = 0, y = 7, label = "noise", col = "blue") +
+  #annotate("segment", x = 0, xend = 0, y = 0, yend = 5, col = "blue", arrow = arrow(ends = "last",length = unit(.2,"cm")))
+p
+
+##### Run DCA #####
